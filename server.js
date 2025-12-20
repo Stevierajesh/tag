7e0633b89214dfd537adc629bd08a8dd1912f8ef
Stevie Rajesh <rajesh.39@osu.edu>
2025-12-20 13:18:23 -0500

    added some game logic, and added some planning

--- server.js
+++ server.js
@@ -1,17 +1,53 @@
+import { gameManager } from './game_engine/gameSessionManagement';
+
+
+const { json } = require('express');
 const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 8080 });
 
 console.log("WebSocket server running on ws://localhost:8080");
-//TEST CODE, NOT FOR PRODUCTION USE
 server.on('connection', socket => {
 
     console.log('New client connected');
     socket.on('message', message => {
         console.log('Received from client: ', message.toString('utf8'));
-        //socket.send(`Rojer That! ${message}`);
+
+        const data = JSON.parse(message);
+
+
+        switch (data.type){
+            case "CREATE_GAME":
+                //function call here, that will start the game and have initial values sent
+                gameManager.gameStart(data.playerID);
+                break;
+            case "JOIN_GAME":
+                
+                break;
+            case "LOCATION_UPDATE":
+                
+                break;
+            case "LEAVE_GAME":
+                
+                break;
+            case "TAG_ATTEMPT":
+                
+                break;
+            case "REVEAL_START":
+                
+                break;
+            case "REVEAL_END":
+                
+                break;
+            default:
+                console.log("ERROR - UNKNOWN EVENT:" + data.type);
+                break;
+        }
+
     });
 
     socket.on('close', () => {
+
+        //Must Add
         console.log('Client disconnected');
     });
 
